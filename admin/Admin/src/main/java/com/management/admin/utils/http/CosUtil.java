80595664f37f22cdb0b48274980961a6555c0ba8
root <2407439@qq.com>
2018-12-28 08:13:20 +0800

    修改阿里云COS工具

--- admin/Admin/src/main/java/com/management/admin/utils/http/CosUtil.java
+++ admin/Admin/src/main/java/com/management/admin/utils/http/CosUtil.java
@@ -11,17 +11,12 @@ import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.model.PutObjectResult;
 import com.management.admin.utils.Base64Util;
 import com.management.admin.utils.JsonUtil;
-import com.qcloud.cos.COSClient;
-import com.qcloud.cos.ClientConfig;
-import com.qcloud.cos.auth.BasicCOSCredentials;
-import com.qcloud.cos.auth.COSCredentials;
-import com.qcloud.cos.model.PutObjectRequest;
-import com.qcloud.cos.region.Region;
 import lombok.val;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -37,8 +32,7 @@ public class CosUtil {
     // 阿里云
     private static OSSClient ossClient;
 
-    // bucket的命名规则为{name}-{appid} ，此处填写的存储桶名称必须为此格式
-    private final static String bucketName = "youxun-1251694474";
+    private final static String bucketName = "yabolive";
 /*    private final static String secretId = "AKIDY0GMH0Rt9IJSQJxXuFcnnbCbnXij4jXN";
     private final static String secretKey = "492zqeMrAJG1riV6bv27yjHn9f4Ii6bw";*/
     // 阿里云
@@ -46,20 +40,7 @@ public class CosUtil {
     private final static String accessKeyId = "LTAIR0oyIQND1tDJ";
     private final static String accessKeySecret = "Xqde9mlClOMyAVVjLPmr9pnaUUX8Wb";
 
-    public static String accessUrl = "https://youxun-1251694474.cos.ap-beijing.myqcloud.com/";
-
-    static {
-/*        // 1 初始化用户身份信息(secretId, secretKey)
-        cosCredentials = new BasicCOSCredentials(secretId, secretKey);
-        // 2 设置bucket的区域, COS地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
-        // clientConfig中包含了设置region, https(默认http), 超时, 代理等set方法, 使用可参见源码或者接口文档FAQ中说明
-        clientConfig = new ClientConfig(new Region("ap-beijing"));
-        // 3 生成cos客户端
-        cosClient = new COSClient(cosCredentials, clientConfig);*/
-        // 创建OSSClient实例。阿里云
-        ossClient = new OSSClient(accessUrl, accessKeyId, accessKeySecret);
-    }
-
+    public static String endpoint = "http://oss-cn-beijing.aliyuncs.com/";
 
     /**
      * 上传文件
@@ -68,56 +49,51 @@ public class CosUtil {
      * @throws Exception
      */
     public static String upload(MultipartFile multipartFile) throws Exception {
-        // 简单文件上传, 最大支持 5 GB, 适用于小文件上传, 建议 20M以下的文件使用该接口
-        // 大文件上传请参照 API 文档高级 API 上传
-        //String encode = Base64Util.encodeFile("C:\\face.jpg");
-        // Base64Util.decodeToFile("C:\\bucket.png", data);
         File localFile = new File("C:\\bucket.png");
-        multipartFile.transferTo(localFile);
-//        FileUtils.copyInputStreamToFile(multipartFile.getInputStream(), localFile);
-        // 指定要上传到 COS 上对象键
-        // 对象键（Key）是对象在存储桶中的唯一标识。例如，在对象的访问域名 `bucket1-1250000000.cos.ap-guangzhou.myqcloud.com/doc1/pic1.jpg` 中，对象键为 doc1/pic1.jpg, 详情参考 [对象键](https://cloud.tencent.com/document/product/436/13324)
-        String key =  "upload/" + UUID.randomUUID().toString() + ".png";
-        // 上传文件流。
-        InputStream inputStream = new FileInputStream(localFile);
-        PutObjectResult putObjectResult = ossClient.putObject(bucketName, key, inputStream);
 
-        logger.debug("COS_" + key + "_" + putObjectResult == null ? "NULL" : JsonUtil.getJson(putObjectResult));
-        return accessUrl + key;
+        multipartFile.transferTo(localFile);
+
+        String key =  "upload/" + UUID.randomUUID().toString() + ".png";
+
+        // 创建OSSClient实例。
+        OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+
+        // 上传内容到指定的存储空间（bucketName）并保存为指定的文件名称（objectName）。
+        InputStream inputStream = new FileInputStream(localFile);
+        ossClient.putObject(bucketName, key, inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+
+        return endpoint + key;
     }
 
-
     /**
-     * 上传文件
+     * 上传base64编码的数据
      * @param data
      * @return
      * @throws Exception
      */
     public static String upload(String data) throws Exception {
-        // 简单文件上传, 最大支持 5 GB, 适用于小文件上传, 建议 20M以下的文件使用该接口
-        // 大文件上传请参照 API 文档高级 API 上传
-        String encode = Base64Util.encodeFile("C:\\face.jpg");
         Base64Util.decodeToFile("C:\\bucket.png", data);
-        File localFile = new File("C:\\bucket.png");
-        // 指定要上传到 COS 上对象键
-        // 对象键（Key）是对象在存储桶中的唯一标识。例如，在对象的访问域名 `bucket1-1250000000.cos.ap-guangzhou.myqcloud.com/doc1/pic1.jpg` 中，对象键为 doc1/pic1.jpg, 详情参考 [对象键](https://cloud.tencent.com/document/product/436/13324)
-        String key =  "face/" + UUID.randomUUID().toString() + ".png";
 
-        // 上传文件流。
+        File localFile = new File("C:\\bucket.png");
+
+        String key =  "upload/" + UUID.randomUUID().toString() + ".png";
+
+        // 创建OSSClient实例。
+        OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+
+        // 上传内容到指定的存储空间（bucketName）并保存为指定的文件名称（objectName）。
         InputStream inputStream = new FileInputStream(localFile);
-        PutObjectResult putObjectResult = ossClient.putObject(bucketName, key, inputStream);
+        ossClient.putObject(bucketName, key, inputStream);
 
         // 关闭OSSClient。
-        //ossClient.shutdown();
+        ossClient.shutdown();
 
-
-/*        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, localFile);
-        PutObjectResult putObjectResult = cosClient.putObject(putObjectRequest);*/
-
-
-
-        logger.debug("COS_" + key + "_" + putObjectResult == null ? "NULL" : JsonUtil.getJson(putObjectResult));
-        return accessUrl + key;
+        return endpoint + key;
     }
+
+
 
 }
